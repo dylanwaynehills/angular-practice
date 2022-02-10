ec2a299774773ffa09c8aa1e9f03f334e29d8d71
dylanwaynehills <dylan@waynehills.co>
2022-02-10 15:48:19 +0900

    todo 목록, 인터페이스 구현

--- angular-todo/src/app/routes/route-todo/route-todo.component.ts
+++ angular-todo/src/app/routes/route-todo/route-todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoList } from '../../todoList'
 
 @Component({
   selector: 'app-route-todo',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouteTodoComponent implements OnInit {
 
+  todoList: TodoList[] | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    // interface TodoList {
+    //   active: boolean,
+    //   content: string,
+    //   time: number
+    // }
+  
+    let todoList: TodoList[] = [
+      { active: false, content: '강아지랑 산책가기', time: 9 },
+      { active: false, content: '쇼미더머니 생방송 챙겨보기', time: 12 },
+      { active: false, content: '친구들 만나기', time: 18 },
+    ];
+    this.todoList = todoList;
+
+    console.log(this.todoList)
   }
 
 }
