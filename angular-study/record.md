cf447ce32eceaff4ca3e6a4d55622edec85258af
dylanwaynehills <dylan@waynehills.co>
2022-02-10 10:51:44 +0900

    mk1-study 프로젝트 생성

---
+++ angular-study/record.md
@@ -0,0 +1,11 @@
+## This is sample markdown page
+
+```javascript
+<sciprt>
+console.log('its working!');
+let test = <T> (arg: T) => {
+    console.log(`name: ${arg} | type: ${T}`)
+}
+</script>
+```
+
---
+++ mk1-study/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LabComponent } from './lab/lab.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LabComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
