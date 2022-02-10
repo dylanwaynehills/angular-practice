da360aff2bd0bc5b6371a0aa88926ab31ab66ef8
dylanwaynehills <dylan@waynehills.co>
2022-02-10 16:12:56 +0900

    interface todoList에서 가져오는 방향으로 수정

--- angular-todo/src/app/routes/route-todo/route-todo.component.ts
+++ angular-todo/src/app/routes/route-todo/route-todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoList } from '../../todoList'
+import { TodoList, todoList } from '../../todoList'
 
 @Component({
   selector: 'app-route-todo',
@@ -10,22 +10,10 @@ export class RouteTodoComponent implements OnInit {
 
   todoList: TodoList[] | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    // interface TodoList {
-    //   active: boolean,
-    //   content: string,
-    //   time: number
-    // }
-  
-    let todoList: TodoList[] = [
-      { active: false, content: '강아지랑 산책가기', time: 9 },
-      { active: false, content: '쇼미더머니 생방송 챙겨보기', time: 12 },
-      { active: false, content: '친구들 만나기', time: 18 },
-    ];
+  ngOnInit(): void {    
     this.todoList = todoList;
-
     console.log(this.todoList)
   }
 
--- angular-todo/src/app/todoList.ts
+++ angular-todo/src/app/todoList.ts
@@ -4,7 +4,8 @@ export interface TodoList {
   time: number
 }
 
-// export {};
-// export const todoList {
-
-// }
+export const todoList = [
+  { active: false, content: '강아지랑 산책가기', time: 9 },
+  { active: false, content: '쇼미더머니 생방송 챙겨보기', time: 12 },
+  { active: false, content: '친구들 만나기', time: 18 },
+]
